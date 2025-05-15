ddb33a1e64ca07e2f466ccc28d9500090d419b2c
Savanazahra18 <147012432+Savanazahra18@users.noreply.github.com>
2025-05-15 18:28:44 +0700

    coba

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, savana!")
+		return c.SendString("Hello, ijah!")
 	})
 
 	app.Listen(":3000")
