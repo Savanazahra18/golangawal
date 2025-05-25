348529abd8849c43c5e51f1ff8412b9d5c50d791
Savanazahra18 <147012432+Savanazahra18@users.noreply.github.com>
2025-05-25 17:20:53 +0700

    menambah struct

---
+++ struct.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Struct Mahasiswa
+type Mahasiswa struct {
+	NPM   int
+	Nama  string
+	Kelas string
+}
+
+// Fungsi yang bekerja dengan struct Mahasiswa
+func GreetMahasiswa(mhs Mahasiswa) string {
+	return fmt.Sprintf("Halo, %s! (NPM: %d) dari kelas %s.", mhs.Nama, mhs.NPM, mhs.Kelas)
+}
+
+func main() {
+	// Membuat instance dari struct Mahasiswa
+	mahasiswa := Mahasiswa{
+		NPM:   714230042,
+		Nama:  "Savana Zahra Humaira",
+		Kelas: "TI-2A",
+	}
+
+	// Memanggil fungsi dan mencetak hasil
+	message := GreetMahasiswa(mahasiswa)
+	fmt.Println(message)
+}
